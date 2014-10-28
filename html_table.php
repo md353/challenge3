6f065398adb8a90d384e34ae4383b8994d1c6c77
IS218 <md353@njit.edu>
2014-10-28 17:21:48 -0400

    Table prints!

--- html_table.php
+++ html_table.php
@@ -15,15 +15,27 @@
 
 		public static $timeline_labels = ['Time and Date of Tweet','Tweet','Tweeted By','Screen name','Followers','Friends','Listed'];
 
-		public static function prTimeline($getfield){
+		public static function prTimeline($getfield, $string){
 
-				echo '<table border="1">';
+				echo '<table border="1"><tr>';
 				$i = 0;
-				foreach(self::$timeline_labels as $label){
-					$records .= '<th>' . self::$timeline_labels[$i] . '</th>';
+				foreach(self::$timeline_labels as $label){					
+					echo '<th>' . self::$timeline_labels[$i] . '</th>';
 					$i++;
-				}				
-					echo $records;
+				}
+					foreach($string as $items){					
+					$tbl .= '<tr>';
+					$tbl .= '<td>' . $items['created_at'] . '</td>';
+					$tbl .= '<td>' . $items['text'] . '</td>';
+					$tbl .= '<td>' . $items['user']['name'] . '</td>';
+					$tbl .= '<td>' . $items['user']['screen_name'] . '</td>';
+					$tbl .= '<td>' . $items['user']['followers_count'] . '</td>';
+					$tbl .= '<td>' . $items['user']['friends_count'] . '</td>';
+					$tbl .= '<td>' . $items['user']['listed_count'] . '</td>';
+					$tbl .= '</tr>';
+				}			
+					$tbl .= '</table>';
+					echo $tbl; 
 	
 		}
 		}
--- index.php
+++ index.php
@@ -8,24 +8,29 @@ require_once('TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
-    'oauth_access_token' => "",
-    'oauth_access_token_secret' => "",
-    'consumer_key' => "",
-    'consumer_secret' => ""
+    'oauth_access_token' => "2800677829-kY3elqM71dH7MJNvZfJzhBOoS4MPf6A387Vy8AV",
+    'oauth_access_token_secret' => "hbLL7ihvw3A6KKHmKjUyHuFSaRrjeRf4dnmbBgwZKfamb",
+    'consumer_key' => "Wt0LSGqyHhHK4U5ATSP3PdFD4",
+    'consumer_secret' => "JSJjyPOIL3E9AZXssCHiVW2rX68n1rkjd2EuRfXrmDR6yIXRBE"
 );
 
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 $requestMethod = "GET";
-if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "Delacruz1021";}
-if (isset($_GET['count'])) {$user = $_GET['count'];} else {$count = 20;}
+	if (isset($_GET['user']))  {
+		$user = $_GET['user'];
+	}else {$user  = "Delacruz1021";
+		}
+	if (isset($_GET['count'])) {
+			$user = $_GET['count'];
+	}else {$count = 20;
+			}
 $getfield = "?screen_name=$user&count=$count";
 $twitter = new TwitterAPIExchange($settings);
-$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
-if($string["errors"][0]["message"] != "") {
-	echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();
-}
-// $content = $connection->get('statuses/user_timeline', array('screen_name' => 'Delacruz1021'));
-html_table::prTimeline($getfield);
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+
+html_table::prTimeline($getfield, $string);
 										  
 ?>										  
-
